20a47cac1435b8ac5bcfb084e718b24f0bf6a91a
lehoangtrieu1 <127284462+lehoangtrieu1@users.noreply.github.com>
2024-04-07 23:32:10 +0800

    added score and text to speech

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 
+
+/* lets keep score! */
+let win = 0;
+let losses = 0;
+let draws = 0;
+
+function say(theMessage){
+    let msg = new SpeechSynthesisUtterance(theMessage);
+    speechSynthesis.speak(msg);
+}
 /* This is a simplified version of the game.
 The Computer ALWAYS picks ROCK.  
 Does the user's choose win,lose, or tie vs ROCK? */
@@ -6,12 +16,20 @@ function play(user) {
     let result = "";
     if (user === 'rock') {
         result = "tie";
+        draws++
     }
     if (user === 'paper') {
         result = "win";
+        wins++
     }
     if (user === 'scissors') {
         result = "lose";
+        losses++
     }
-    alert("Computer chose rock, you " + result + "!");
+    alert("Computer chose rock, you " + result + "!" +
+"/n/n" + wins + " wins" +
+"/n"   + losses + "losses" +
+"/n"   + draws + "draws");
+alert(myMessage)
+say(myMessage)
 }
